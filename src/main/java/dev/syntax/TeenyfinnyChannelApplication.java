49f48bcb4a2c7d4ee877b5ee89d2036095acb682
JBLee <rabid28@naver.com>
2025-11-11 17:41:15 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/dev/syntax/TeenyfinnyChannelApplication.java
@@ -0,0 +1,13 @@
+package dev.syntax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeenyfinnyChannelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeenyfinnyChannelApplication.class, args);
+    }
+
+}
