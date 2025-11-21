37b27eee542ffb1844fb1cad62310221d65e0c6c
yes2489 <yes24150@gmail.com>
2025-11-21 14:53:05 +0900

    feat: WebMvcConfig에서 Resolver 등록

--- src/main/java/dev/syntax/global/config/WebConfig.java
+++ src/main/java/dev/syntax/global/config/WebConfig.java
@@ -1,12 +1,23 @@
 package dev.syntax.global.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import dev.syntax.global.auth.resolver.CurrentUserArgumentResolver;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override public void addCorsMappings(CorsRegistry r) {
-        r.addMapping("/**").allowedMethods("GET","POST","PUT","DELETE").allowedOrigins("http://localhost:3000");
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry r) {
+		r.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("http://localhost:3000");
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(new CurrentUserArgumentResolver());
+	}
 }
