b870dfac47775839c32933f9bf0b230220091c1c
yes2489 <yes24150@gmail.com>
2025-11-21 14:33:48 +0900

    feat: 로그인 응답에 토큰타입 추가

--- src/main/java/dev/syntax/domain/auth/dto/LoginRes.java
+++ src/main/java/dev/syntax/domain/auth/dto/LoginRes.java
@@ -6,11 +6,13 @@ import lombok.Builder;
 /**
  * 로그인 응답 DTO.
  *
+ * @param tokenType   토큰 종류
  * @param accessToken 발급된 JWT 액세스 토큰
  * @param user        로그인한 사용자 정보
  */
 @Builder
 public record LoginRes(
+	String tokenType,
 	String accessToken,
 	UserLoginInfo user
 ) {
@@ -19,6 +21,7 @@ public record LoginRes(
 
 	public static LoginRes of(@NotNull UserContext userContext, String accessToken) {
 		return LoginRes.builder()
+			.tokenType(DEFAULT_TOKEN_TYPE)
 			.accessToken(accessToken)
 			.user(
 				UserLoginInfo.builder()
