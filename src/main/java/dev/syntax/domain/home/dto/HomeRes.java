69341183089a69da87546b7853ee83b59e765b96
양은서 <dmstj4871@naver.com>
2025-11-23 14:34:51 +0900

    feat: 홈(대시보드) 조회 DTO 구현

---
+++ src/main/java/dev/syntax/domain/home/dto/HomeRes.java
@@ -0,0 +1,49 @@
+package dev.syntax.domain.home.dto;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import dev.syntax.domain.user.enums.Role;
+import lombok.Builder;
+
+/**
+ * 홈 화면 API 응답 DTO입니다.
+ */
+@Builder
+public record HomeRes(
+	UserDto user
+) {
+
+	/**
+	 * 사용자 정보 및 역할별 추가 데이터를 포함하는 DTO입니다.
+	 */
+	@Builder
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	public record UserDto(
+		Long userId,
+		String name,
+		Role role,
+		String email,
+		Long balance, // 부모
+		List<ChildDto> children, // 부모
+
+		Long totalBalance, // 자녀
+		Long depositBalance, // 자녀
+		Long investmentBalance, // 자녀
+		Long savingBalance // 자녀
+	) {
+	}
+
+	/**
+	 * 부모가 조회할 때 포함되는 자녀 정보 DTO입니다.
+	 */
+	@Builder
+	public record ChildDto(
+		Long userId,
+		String name,
+		Long balance,
+		Byte gender
+	) {
+	}
+}
