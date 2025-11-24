4e1c20132c1cf4e0ba141a8e64952e95db5ec469
hyojeongbae <bhj09270@naver.com>
2025-11-24 19:49:31 +0900

    refactor: 숫자 포맷팅 utils 적용

--- src/main/java/dev/syntax/domain/goal/dto/GoalDetailRes.java
+++ src/main/java/dev/syntax/domain/goal/dto/GoalDetailRes.java
@@ -1,23 +1,50 @@
 package dev.syntax.domain.goal.dto;
 
-import lombok.AllArgsConstructor;
+import dev.syntax.global.service.Utils;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-@AllArgsConstructor
 public class GoalDetailRes {
 
     private Long goalId;
     private Long userId;
     private String name;
-    private BigDecimal targetAmount;
-    private BigDecimal currentAmount;
+    private String targetAmount;
+    private String currentAmount;
     private int period;
     private int progress;
     private String userName;
-    private List<BigDecimal> depositAmount;
+    private List<String> depositAmount;
     private List<String> depositDatetime;
+
+    public GoalDetailRes(
+            Long goalId,
+            Long userId,
+            String name,
+            BigDecimal targetAmount,
+            BigDecimal currentAmount,
+            int period,
+            int progress,
+            String userName,
+            List<BigDecimal> depositAmount,
+            List<String> depositDatetime
+    ) {
+        this.goalId = goalId;
+        this.userId = userId;
+        this.name = name;
+        this.targetAmount = Utils.NumberFormattingService(targetAmount.intValue());
+        this.currentAmount = Utils.NumberFormattingService(currentAmount.intValue());
+        this.period = period;
+        this.progress = progress;
+        this.userName = userName;
+        this.depositAmount = depositAmount.stream()
+                .map(v -> Utils.NumberFormattingService(v.intValue()))
+                .collect(Collectors.toList());
+
+        this.depositDatetime = depositDatetime;
+    }
 }
--- src/main/java/dev/syntax/domain/goal/dto/GoalInfoRes.java
+++ src/main/java/dev/syntax/domain/goal/dto/GoalInfoRes.java
@@ -1,24 +1,24 @@
 package dev.syntax.domain.goal.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.syntax.domain.goal.entity.Goal;
+import dev.syntax.global.service.Utils;
 import lombok.Getter;
-
-import java.math.BigDecimal;
 
 @Getter
 public class GoalInfoRes {
 
     private Long id;
     private String name;
-    private BigDecimal targetAmount;
-    private BigDecimal monthlyAmount;
+    private String targetAmount;
+    private String monthlyAmount;
     private Integer payDay;
 
     public GoalInfoRes(Goal goal) {
         this.id = goal.getId();
         this.name = goal.getName();
-        this.targetAmount = goal.getTargetAmount();
-        this.monthlyAmount = goal.getMonthlyAmount();
+        this.targetAmount = Utils.NumberFormattingService(goal.getTargetAmount().intValue());
+        this.monthlyAmount = Utils.NumberFormattingService(goal.getMonthlyAmount().intValue());
         this.payDay = goal.getPayDay();
     }
 }
