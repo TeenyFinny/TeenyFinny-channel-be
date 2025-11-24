bd97bcccf83b27abbc0e533b78dbb96115d998fa
hyojeongbae <bhj09270@naver.com>
2025-11-24 12:29:23 +0900

    feat: dto 유효성 검사 어노테이션 추가

--- src/main/java/dev/syntax/domain/goal/dto/GoalCreateReq.java
+++ src/main/java/dev/syntax/domain/goal/dto/GoalCreateReq.java
@@ -1,5 +1,6 @@
 package dev.syntax.domain.goal.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 import java.math.BigDecimal;
@@ -7,8 +8,19 @@ import java.math.BigDecimal;
 @Getter
 public class GoalCreateReq {
 
+    @NotBlank(message = "목표 이름은 비워둘 수 없습니다.")
     private String name;
+
+    @NotNull(message = "목표 금액은 비워둘 수 없습니다.")
+    @Positive(message = "목표 금액은 0보다 커야 합니다.")
     private BigDecimal targetAmount;
+
+    @NotNull(message = "월 납입 금액은 비워둘 수 없습니다.")
+    @Positive(message = "월 납입 금액은 0보다 커야 합니다.")
     private BigDecimal monthlyAmount;
+
+    @NotNull(message = "납입일은 비워둘 수 없습니다.")
+    @Min(value = 1, message = "납입일은 1일 이상이어야 합니다.")
+    @Max(value = 31, message = "납입일은 31일 이하이어야 합니다.")
     private Integer payDay;
 }
