0dd6016a3b7f60724261fe27b8ce1656ba5e4400
CHICHIT <beflylee@naver.com>
2025-11-25 12:21:19 +0900

    refactor: 패스워드 검증 로직 추가

--- src/main/java/dev/syntax/domain/auth/dto/PasswordVerifyReq.java
+++ src/main/java/dev/syntax/domain/auth/dto/PasswordVerifyReq.java
@@ -1,7 +1,14 @@
 package dev.syntax.domain.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record PasswordVerifyReq(
-        @NotBlank String password
+        @NotBlank
+        @Pattern(
+                regexp = "^(?=.*[!@#$%^&*(),.?\":{}|<>]).{8,}$",
+                message = "비밀번호는 8자리 이상이어야 하며, 특수문자를 포함해야 합니다."
+        )
+        String password
+
 ) { }
