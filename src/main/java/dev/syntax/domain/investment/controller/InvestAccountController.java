9b91a588ff56d008b3a01d8d663d0b0b3ed6d74e
양은서 <dmstj4871@naver.com>
2025-11-26 00:46:09 +0900

    refactor: 다른 PR들과 동일하게 controller 이름 수정

--- src/main/java/dev/syntax/domain/investment/controller/InvestAccountController.java
+++ src/main/java/dev/syntax/domain/investment/controller/InvestAccountController.java
@@ -1,11 +1,5 @@
 package dev.syntax.domain.investment.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import dev.syntax.domain.investment.dto.res.AccountRes;
 import dev.syntax.domain.investment.service.AccountService;
 import dev.syntax.global.auth.annotation.CurrentUser;
@@ -14,11 +8,16 @@ import dev.syntax.global.response.ApiResponseUtil;
 import dev.syntax.global.response.BaseResponse;
 import dev.syntax.global.response.SuccessCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/investments")
-public class InvestmentAccountController {
+public class InvestAccountController {
 	private final AccountService accountService;
 
 	@GetMapping("/account")
