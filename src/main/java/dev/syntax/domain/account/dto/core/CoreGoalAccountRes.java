173ab23625d7b8c80537a99676f6ce8a759c8228
CHICHIT <33713817+CHICHIT@users.noreply.github.com>
2025-11-26 01:22:41 +0900

    fix: 어노테이션 수정

--- src/main/java/dev/syntax/domain/account/dto/core/CoreGoalAccountRes.java
+++ src/main/java/dev/syntax/domain/account/dto/core/CoreGoalAccountRes.java
@@ -1,12 +1,11 @@
 package dev.syntax.domain.account.dto.core;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CoreGoalAccountRes {
