620cd30424fb35732005063f98661ef27f5c3b93
yes2489 <yes24150@gmail.com>
2025-11-25 13:59:04 +0900

    chore: DTO 위치 수정

--- src/main/java/dev/syntax/domain/user/dto/CoreParentInitRes.java
+++ src/main/java/dev/syntax/domain/user/dto/CoreParentInitRes.java
@@ -1,8 +1,8 @@
 package dev.syntax.domain.user.dto;
 
-import dev.syntax.domain.account.dto.CoreAccountItemRes;
+import dev.syntax.domain.account.dto.core.CoreAccountItemRes;
 
 public record CoreParentInitRes(Long coreUserId,
-                                CoreAccountItemRes account
+								CoreAccountItemRes account
 ) implements CoreInitRes {
 }
