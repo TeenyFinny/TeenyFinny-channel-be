4cb8d0e18fab271f40194bb697ace4f8cf70c8b4
yes2489 <yes24150@gmail.com>
2025-11-25 13:58:27 +0900

    refactor: balance - BigDecimal 타입으로 수정

--- src/main/java/dev/syntax/domain/account/dto/core/CoreAccountItemRes.java
+++ src/main/java/dev/syntax/domain/account/dto/core/CoreAccountItemRes.java
@@ -1,7 +1,9 @@
-package dev.syntax.domain.account.dto;
+package dev.syntax.domain.account.dto.core;
+
+import java.math.BigDecimal;
 
 public record CoreAccountItemRes(Long accountId,
 								 String accountNumber,
 								 String accountType,
-								 String balance) {
+								 BigDecimal balance) {
 }
