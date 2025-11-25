f640a3564a28a6a41cc44c0904cd6b76204106eb
yes2489 <yes24150@gmail.com>
2025-11-25 10:41:22 +0900

    refactor: 응답 DTO 수정

--- src/main/java/dev/syntax/domain/account/dto/CoreAccountItemRes.java
+++ src/main/java/dev/syntax/domain/account/dto/CoreAccountItemRes.java
@@ -1,9 +1,7 @@
 package dev.syntax.domain.account.dto;
 
-import dev.syntax.domain.account.enums.AccountType;
-
 public record CoreAccountItemRes(Long accountId,
-                                 String accountNumber,
-                                 AccountType accountType,
-                                 String balance) {
+								 String accountNumber,
+								 String accountType,
+								 String balance) {
 }
