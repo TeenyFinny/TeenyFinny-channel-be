d0cf3889b2ce8a60735f75f4b96309c45264eb1b
yes2489 <yes24150@gmail.com>
2025-11-25 20:43:59 +0900

    fix: Bean 중복 등록 컨트롤러 이름 변경

---
+++ src/main/java/dev/syntax/domain/investment/controller/InvestmentAccountController.java
@@ -0,0 +1,39 @@
+package dev.syntax.domain.investment.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.syntax.domain.investment.dto.res.AccountRes;
+import dev.syntax.domain.investment.service.AccountService;
+import dev.syntax.global.auth.annotation.CurrentUser;
+import dev.syntax.global.auth.dto.UserContext;
+import dev.syntax.global.response.ApiResponseUtil;
+import dev.syntax.global.response.BaseResponse;
+import dev.syntax.global.response.SuccessCode;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/investments")
+public class InvestmentAccountController {
+	private final AccountService accountService;
+
+	@GetMapping("/account")
+	public ResponseEntity<BaseResponse<?>> getCanoByUserId(
+		@CurrentUser UserContext userContext
+	) {
+		String res = accountService.getCanoByUserId(userContext.getId());
+		return ApiResponseUtil.success(SuccessCode.OK, res);
+	}
+
+	@PostMapping("/account")
+	public ResponseEntity<BaseResponse<?>> createInvestmentAccount(
+		@CurrentUser UserContext userContext
+	) {
+		AccountRes res = accountService.createInvestmentAccount(userContext.getId());
+		return ApiResponseUtil.success(SuccessCode.CREATED, res);
+	}
+}
