d3e1fe727598c8e5b4639fe71853230d021b3d38
hyojeongbae <bhj09270@naver.com>
2025-11-26 15:24:18 +0900

    feat: 계좌 상태 변경 core api 연동

--- src/main/java/dev/syntax/domain/goal/client/CoreGoalClient.java
+++ src/main/java/dev/syntax/domain/goal/client/CoreGoalClient.java
@@ -1,6 +1,8 @@
 package dev.syntax.domain.goal.client;
 
 import dev.syntax.domain.goal.dto.CoreTransactionHistoryRes;
+import dev.syntax.domain.goal.dto.CoreUpdateAccountStatusReq;
+import dev.syntax.domain.goal.dto.CoreUpdateAccountStatusRes;
 import dev.syntax.global.core.CoreApiProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
@@ -15,11 +17,21 @@ public class CoreGoalClient {
     private final CoreApiProperties properties;
 
     private final String GET_HISTORY_URL = "/core/transaction/account/";
+    private final String UPDATE_ACCOUNT_STATUS_URL = "/core/banking/account/{accountNo}/status";
 
     public CoreTransactionHistoryRes getAccountHistory(String accountNo) {
         return coreRestTemplate.getForObject(
                 properties.getBaseUrl() + GET_HISTORY_URL + "{accountNo}",
                 CoreTransactionHistoryRes.class,
+                accountNo
+        );
+    }
+
+    public CoreUpdateAccountStatusRes updateAccountStatus(String accountNo, CoreUpdateAccountStatusReq req) {
+        return coreRestTemplate.postForObject(
+                properties.getBaseUrl() + UPDATE_ACCOUNT_STATUS_URL,
+                req,
+                CoreUpdateAccountStatusRes.class,
                 accountNo
         );
     }
