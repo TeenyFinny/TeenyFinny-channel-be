77ba3e065defb37b9c46514994c87a30c1bbf815
hyojeongbae <bhj09270@naver.com>
2025-11-27 16:02:20 +0900

    feat: 목표 계좌 자동이체 채널+코 i êµ구현 및 코어 연동

--- src/main/java/dev/syntax/domain/transfer/repository/AutoTransferRepository.java
+++ src/main/java/dev/syntax/domain/transfer/repository/AutoTransferRepository.java
@@ -1,8 +1,10 @@
 package dev.syntax.domain.transfer.repository;
 
+import dev.syntax.domain.account.entity.Account;
 import dev.syntax.domain.transfer.entity.AutoTransfer;
 import dev.syntax.domain.transfer.enums.AutoTransferType;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -49,4 +51,10 @@ public interface AutoTransferRepository extends JpaRepository<AutoTransfer, Long
     Optional<AutoTransfer> findByUserIdAndType(Long childId, AutoTransferType type);
 
 
+    List<AutoTransfer> findAllByAccountId(Long accountId);
+
+
+    List<AutoTransfer> account(Account account);
+
+    Optional<AutoTransfer> findByAccountIdAndType(Long accountId, AutoTransferType type);
 }
---
+++ src/main/java/dev/syntax/domain/transfer/service/AutoTransferServiceImpl.java
@@ -0,0 +1,38 @@
+package dev.syntax.domain.transfer.service;
+
+import dev.syntax.domain.account.repository.AccountRepository;
+import dev.syntax.domain.transfer.client.CoreTransferClient;
+import dev.syntax.domain.transfer.entity.AutoTransfer;
+import dev.syntax.domain.transfer.enums.AutoTransferType;
+import dev.syntax.domain.transfer.repository.AutoTransferRepository;
+import dev.syntax.domain.user.entity.User;
+import dev.syntax.global.auth.dto.UserContext;
+import dev.syntax.global.exception.BusinessException;
+import dev.syntax.global.response.error.ErrorBaseCode;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AutoTransferServiceImpl implements AutoTransferService {
+
+    private final AutoTransferRepository autoTransferRepository;
+    private final AccountRepository accountRepository;
+    private final CoreTransferClient coreTransferClient;
+
+    @Override
+    @Transactional
+    public void deleteAutoTransfer(Long accountId, AutoTransferType type) {
+
+        AutoTransfer autoTransfer = autoTransferRepository.findByAccountIdAndType(accountId, type)
+                .orElseThrow(() -> new BusinessException(ErrorBaseCode.AUTO_TRANSFER_NOT_FOUND));
+
+        coreTransferClient.deleteAutoTransfer(autoTransfer.getPrimaryBankTransferId());
+        autoTransferRepository.delete(autoTransfer);
+    }
+}
