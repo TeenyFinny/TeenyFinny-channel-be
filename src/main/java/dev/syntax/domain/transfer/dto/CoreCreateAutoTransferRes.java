11a494158916539ba596fddc52c0e71ad78ccae6
Park Minseo <eo77si9@naver.com>
2025-11-28 09:49:44 +0900

    chore: 파일명 수정

--- src/main/java/dev/syntax/domain/transfer/dto/CoreCreateAutoTransferRes.java
+++ src/main/java/dev/syntax/domain/transfer/dto/CoreCreateAutoTransferRes.java
@@ -4,6 +4,6 @@ package dev.syntax.domain.transfer.dto;
  * Core 서버에서 자동이체 생성 후 반환되는 응답 DTO입니다.
  * @param autoTransferId Core 서버에 생성된 자동이체 ID
  */ 
-public record CoreAutoTransferRes (
+public record CoreCreateAutoTransferRes (
     Long autoTransferId
 ){}
