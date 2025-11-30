18a2e671571204f93f5d8de13e0637689cb0a738
양은서 <dmstj4871@naver.com>
2025-12-01 01:46:24 +0900

    fix: build 에러 수정

--- src/test/java/dev/syntax/domain/investment/service/AccountServiceImplTest.java
+++ src/test/java/dev/syntax/domain/investment/service/AccountServiceImplTest.java
@@ -9,8 +9,6 @@ import org.mockito.*;
 
 import org.springframework.web.client.RestTemplate;
 
-import java.math.BigDecimal;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -34,7 +32,7 @@ class AccountServiceImplTest {
     @Test
     void 투자계좌생성_성공() {
         Long userId = 1L;
-        CoreInvestmentAccountRes mockRes = new CoreInvestmentAccountRes("12345678",1L,new BigDecimal(0));
+        CoreInvestmentAccountRes mockRes = new CoreInvestmentAccountRes("12345678", 1L, 0L);
 
         when(coreRestTemplate.postForObject(
                 properties.getBaseUrl() + "/core/banking/account/investment?userId=" + userId,
