2df0f89f99336496c302c9d509e40e9df12034ca
CHICHIT <beflylee@naver.com>
2025-12-01 11:03:03 +0900

    refactor: enum 조정

--- src/main/java/dev/syntax/domain/account/enums/AccountType.java
+++ src/main/java/dev/syntax/domain/account/enums/AccountType.java
@@ -1,8 +1,11 @@
 package dev.syntax.domain.account.enums;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 public enum AccountType {
 	DEPOSIT,
 	ALLOWANCE,
 	GOAL,
+    @JsonAlias("INVESTMENT")
 	INVEST
 }
